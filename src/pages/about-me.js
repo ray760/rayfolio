1eae2fba89cd4690a95599bedaa6749dd0af2d22
Ray Robertson <rayrobertson@Rays-MacBook-Pro.local>
2020-06-04 00:32:07 -0400

    Updated package.json file and packages.
    Created travis.yml file.
    
    Commented out some error messages.

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+// import Layout from '../components/layout'
+// import SEO from '../components/seo'
 
 const SecondPage = () => (
   <React.Fragment>
@@ -12,15 +12,31 @@ const SecondPage = () => (
     <div className="bg">
       <div className="picture about">
         <p>
-          Hi, my name is Ray Robertson and I'm into Front End Dev. Currently, I reside in the "V" in the <a href="https://bit.ly/2GPDb0M">D.M.V</a> but born and raised in Kings County, Brooklyn, NY. I'm a Son, Brother, Husband, and Father of two amazing boys.         
+          Hi, my name is Ray Robertson and I'm into Front End Dev. Currently, I
+          reside in the "V" in the <a href="https://bit.ly/2GPDb0M">D.M.V</a>{' '}
+          but born and raised in Kings County, Brooklyn, NY. I'm a Son, Brother,
+          Husband, and Father of two amazing boys.
         </p>
 
         <p>
-          I currently work at <a href="https://www.trusted.com">Trusted</a>, where I spend time taking RESTful API data and turning it into Front End, Client-side goodness. I'm a React fan and spend time keeping update to date with new React releases, staying sharp with Redux, create-react-app and working on my <a href="https://github.com/ray760/reactformbuilder">side project</a> where I add and implement new things I've learned. I also listen to some of my favorite industry podcasts like <a href="https://reactpodcast.simplecast.fm/">React Podcast</a>, <a href="https://syntax.fm/">Syntax</a> and <a href="https://frontendhappyhour.com/">Front End Happy Hour.</a>
+          I currently work at <a href="https://www.trusted.com">Trusted</a>,
+          where I spend time taking RESTful API data and turning it into Front
+          End, Client-side goodness. I'm a React fan and spend time keeping
+          update to date with new React releases, staying sharp with Redux,
+          create-react-app and working on my{' '}
+          <a href="https://github.com/ray760/reactformbuilder">side project</a>{' '}
+          where I add and implement new things I've learned. I also listen to
+          some of my favorite industry podcasts like{' '}
+          <a href="https://reactpodcast.simplecast.fm/">React Podcast</a>,{' '}
+          <a href="https://syntax.fm/">Syntax</a> and{' '}
+          <a href="https://frontendhappyhour.com/">Front End Happy Hour.</a>
         </p>
 
         <p>
-          Check out my <a href="https://www.linkedin.com/in/rayarobertson">LinkedIn</a>, <a href="https://github.com/ray760">Github</a> or shot me an Email to discuss any opportunities or anything developer related.
+          Check out my{' '}
+          <a href="https://www.linkedin.com/in/rayarobertson">LinkedIn</a>,{' '}
+          <a href="https://github.com/ray760">Github</a> or shot me an Email to
+          discuss any opportunities or anything developer related.
         </p>
       </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
+// import Layout from '../components/layout'
+// import Image from '../components/image'
+// import SEO from '../components/seo'
 
 const IndexPage = () => (
   <div className="bg">
@@ -12,12 +12,26 @@ const IndexPage = () => (
       <span className="slim-text">FRONTEND</span>
       <span className="bold-text dev">DEVELOPER</span>
       <div className="links">
-        <span className="slim-text"><Link to="/about-me/">ABOUT</Link></span>
-        <span className="bold-text"><a href="https://github.com/ray760?tab=repositories">GITHUB</a></span>
-        <span className="slim-text"><a href="https://www.linkedin.com/in/rayarobertson">LINKEDIN</a></span>
-        <span className="bold-text"><a href="mailto:ray760@yahoo.com?Subject=Hey%20Ray%20Let's%20Chat%20Front%20Development">EMAIL</a></span>
-        <span className="slim-text"><a href="http://www.rayfolio.com/docs/Ray-Robertson-Resume.pdf">resume</a></span>
-      </div>        
+        <span className="slim-text">
+          <Link to="/about-me/">ABOUT</Link>
+        </span>
+        <span className="bold-text">
+          <a href="https://github.com/ray760?tab=repositories">GITHUB</a>
+        </span>
+        <span className="slim-text">
+          <a href="https://www.linkedin.com/in/rayarobertson">LINKEDIN</a>
+        </span>
+        <span className="bold-text">
+          <a href="mailto:ray760@yahoo.com?Subject=Hey%20Ray%20Let's%20Chat%20Front%20Development">
+            EMAIL
+          </a>
+        </span>
+        <span className="slim-text">
+          <a href="http://www.rayfolio.com/docs/Ray-Robertson-Resume.pdf">
+            resume
+          </a>
+        </span>
+      </div>
     </div>
   </div>
   // <Layout>
