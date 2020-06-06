2462dfbabf98bb0422014af7e9577371f07d409a
Ray Robertson <rayrobertson@Rays-MacBook-Pro.local>
2020-06-05 21:08:44 -0400

    Updated pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  pathPrefix: '/rayfolio/',
+  pathPrefix: `/rayfolio`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Rayfolio.com | Ray Robertson`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
